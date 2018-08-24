00e4edcfbd80fc54b6aaeda8d7b30effd1efae65
Franck-Pro <franck.fernandez@gmail.com>
2018-08-24 15:01:22 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = function () {
+    return <div>Salut les amis</div>
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
+
