7e0ddc6c6b011c618831f34eae0ba3575161dc52
Franck-Pro <franck.fernandez@gmail.com>
2018-08-24 15:30:08 +0200

    add API et class component pour searchbar

---
+++ src/containers/app.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import SearchBar from '../components/search-bar'
+
+const App = function () {
+    return <div><SearchBar/></div>
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './containers/app'
 
-const App = function () {
-    return <div>Salut les amis</div>
-}
+const API_KEY = "49a7c834e4ee728f7194deb5586b9d39";
+
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
 
