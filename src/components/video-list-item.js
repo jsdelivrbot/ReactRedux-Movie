12d2e0ed0d7c13e6d35341ec24fcd6ab88b6afd2
Franck-Pro <franck.fernandez@gmail.com>
2018-09-04 15:02:12 +0200

    API Titre et img

--- src/components/video-list-item.js
+++ src/components/video-list-item.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
+
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/";
+
 const VideoListItem = ({movie}) => {
-    return <li>Un flim recommandé : {movie}</li>
+    return <li>
+                <img src={`${IMAGE_BASE_URL}${movie.poster_path}`} width="100px" height="100px"/>
+                <h3>Un flim recommandé : {movie.title}</h3>
+            </li>
 }
 
 export default VideoListItem;
--- src/containers/app.js
+++ src/containers/app.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import SearchBar from '../components/search-bar'
 import VideoList from './video-list'
+import VideoDetail from '../components/video-detail'
 import axios from 'axios'
 
 
@@ -17,20 +18,49 @@ class App extends Component {
     }
 
     componentWillMount () {
+        this.initMovies();
+    }
+
+    initMovies (){
         axios.get(`${API_END_POINT}${POPULAR_MOVIES_URL}&${API_KEY}`).then(function(response){
-            this.setState({movieList:response.data.results.slice(1,6)});
-            this.setState({currentMovie:response.data.results[0]})
-            console.log("-----------");
-            console.log("", this.state.movieList);
-            console.log("-----------");
+            this.setState({movieList:response.data.results.slice(1,6),currentMovie:response.data.results[0]}, function(){
+                this.applyVideoToCurrentMovie();
+            });
+        }.bind(this));
+    }
+
+
+    applyVideoToCurrentMovie () {
+
+        axios.get(`${API_END_POINT}movie/${this.state.currentMovie.id}?${API_KEY}&append_to_response=videos&include_adult=false`).then(function(response){
+            console.log("-------",);
+            console.log("",response);
+            console.log("-------",);
+
+            const youtubeKey = response.data.videos.results[0].key;
+            let newCurrentMovieState = this.state.currentMovie;
+            newCurrentMovieState.videoId = youtubeKey;
+            this.setState({currentMovie : newCurrentMovieState});
+
+            console.log("-------",);
+            console.log("",newCurrentMovieState);
+            console.log("-------",);
         }.bind(this));
     }
 
     render () {
+
+        const renderVideoList = () => {
+            if (this.state.movieList.length>=5) {
+                return <VideoList movieList={this.state.movieList}/>
+            }
+        }
+
         return (
             <div>
                 <SearchBar/>
-                <VideoList/>
+                {renderVideoList()}
+                <VideoDetail title={this.state.currentMovie.title} description={this.state.currentMovie.overview} />
             </div>
         )
     }
